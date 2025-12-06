12739bb9da53730e7394f24765005b23a8005cc0
Katea3 <ecaterina.muntean23@gmail.com>
2025-12-06 13:38:58 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,28 @@
-﻿var library = new Library
+﻿int[] array = new int [3];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
 {
-    books = new Book[5],
-};
-
-library.books[0] = new Book { name = "test", year = 1929, status = true };
-library.books[1] = new Book { name = "testBook2", year = 1930, status = true };
-library.books[2] = new Book { name = "testBook3", year = 1931, status = true };
-library.books[3] = new Book { name = "testBook4", year = 1932, status = true };
-library.books[4] = new Book { name = "testBook5", year = 1933, status = false };
-
-foreach (var b in library.books)
-{
-    Console.WriteLine(b.name.Length);
-}
-
-library.ShowAllBook();
-
-library.TakeBook("testBook3");
-library.ShowAllBook();
-
-
-library.BusyBook();
-
-library.ReturnBook("testBook3");
-library.ShowAllBook();
-
-library.BusyBook();
-class Library
-{
-    public required Book[] books;
-
-    public void ShowAllBook()
+    while (true)
     {
-        foreach (var book in books)
+        Console.WriteLine($"Введите число №{i + 1}");
+        string number = Console.ReadLine()!;
+
+        if (int.TryParse(number, out int num) && num >= 2)
         {
-            string statusInfo = book.status ? "В наличии" : "Занято";
-            Console.WriteLine($"{book.name} ({book.year} - Статус: {statusInfo})");
+            array[i] = num;
+            break;
         }
-    }
-
-    public void TakeBook(string name)
-    {
-        foreach (var book in books)
+        else
         {
-            if (book.name == name)
-            {
-                if (!book.status)
-                {
-                    Console.WriteLine("Книга занята");
-                    return;
-                }
-                book.status = false;
-                Console.WriteLine($"Ты взял книгу {book.name}");
-                return;
-            }
-        }
-
-        Console.WriteLine("Книга не найдена");
-        return;
-    }
-    public void ReturnBook(string name)
-    {
-        foreach (var book in books)
-        {
-            if (book.name == name)
-            {
-                if (book.status)
-                {
-                    Console.WriteLine("Книга уже в библиотеке");
-                    return;
-                }
-                book.status = true;
-                Console.WriteLine($"Вы вернули книгу {book.name}");
-                return;
-            }
-        }
-        Console.WriteLine("Книга не найдена");
-        return;
-    }
-    public void BusyBook()
-    {
-        var busyBook = new List<string>();
-
-        foreach (var book in books)
-        {
-            if (book.status == false)
-            {
-                busyBook.Add(book.name);
-            }
-        }
-
-
-        Console.WriteLine("Занятые книги:");
-
-        foreach (var busy in busyBook)
-        {
-            Console.WriteLine(busy);
+            Console.WriteLine("Введите число которое больше 2");
         }
     }
 }
 
-class Book
+foreach (int num in array)
 {
-    public required string name;
-    public int year;
-    public bool status;
+    sum += num;
 }
+
+Console.WriteLine($"Сумма чисел равна: {sum}");
